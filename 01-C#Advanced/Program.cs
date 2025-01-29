12bc40ee02a03c09ad55ebfadcf6ff887f6d6d80
Islam <islamcobra00@gmail.com>
2025-01-29 21:15:40 +0200

    Add project files.

---
+++ 01-C#Advanced/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_C_Advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
